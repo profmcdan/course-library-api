be3c94fb95cac87d659633112fe897499926db9f
Daniel Ale <danielale9291@gmail.com>
2020-11-10 08:45:30 +0100

    support xml

--- CourseLibrary/Startup.cs
+++ CourseLibrary/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -28,7 +29,11 @@ namespace CourseLibrary
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers(setupAction =>
+            {
+                setupAction.ReturnHttpNotAcceptable = true;
+            }).AddXmlDataContractSerializerFormatters();
+            
             services.AddScoped<ICourseLibraryRepository, CourseLibraryRepository>();
             services.AddDbContext<CourseLibraryContext>(options =>
             {
